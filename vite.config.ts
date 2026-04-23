8be06a7d23a169d23adecaee6e5b514cfeca8d17
gloedige <gerd.loedige@posteo.de>
2026-04-23 12:46:46 +0200

    create first draw of settings page + install node

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,21 @@
 import {defineConfig} from 'vite'
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-    base:"" 
+    base:"./",
+    
+    build: {
+    rollupOptions: {
+      input: {
+        // Hauptseite
+        main: resolve(__dirname, 'index.html'),
+        // Hier fügst du deine HTML-Dateien aus dem Unterordner hinzu
+        settings: resolve(__dirname, 'src/html/settings.html'),
+      },
+    },
+  },
 });
