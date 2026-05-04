52c9cf71b7643c68eaeba794a70400596de14aeb
gloedige <gerd.loedige@posteo.de>
2026-05-04 21:22:11 +0200

    update gitignore und die vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-    base:"./",
+    base:"/Memory/",
     
     build: {
     rollupOptions: {
@@ -15,6 +15,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         // Hier fügst du deine HTML-Dateien aus dem Unterordner hinzu
         settings: resolve(__dirname, 'src/html/settings.html'),
+        game_board: resolve(__dirname, 'src/html/game_board.html'),
+        game_over: resolve(__dirname, 'src/html/game_over.html'),
+        winner: resolve(__dirname, 'src/html/winner.html'),
       },
     },
   },
